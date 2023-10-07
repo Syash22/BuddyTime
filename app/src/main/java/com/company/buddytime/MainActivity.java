e6e49061a0d2c16d78f4fa06f91bceb041582ad1
DESKTOP-LJEG5IQ\Administrator <amin0821@naver.com>
2023-10-07 19:00:29 +0900

    feat: login_trans_signup

--- app/src/main/java/com/company/buddytime/MainActivity.java
+++ app/src/main/java/com/company/buddytime/MainActivity.java
@@ -2,13 +2,28 @@ package com.company.buddytime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_signin);
+
+        text = (TextView) findViewById(R.id.textView);
+        text.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), transepage.class);
+                startActivity(intent);
+
+            }
+        }
+        );
     }
 }
